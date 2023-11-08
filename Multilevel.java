8b5d1dedec083cc4ec5f3b324104edc6b81b7c61
OlivierNir <149686325+OlivierNir@users.noreply.github.com>
2023-11-08 19:09:05 +0200

    Add files via upload

---
+++ Multilevel.java
@@ -0,0 +1,23 @@
+class A{  
+void primary(){System.out.println("Nyarubande");}  
+}  
+class B extends A{  
+void olevel(){System.out.println("Burema");}  
+}  
+class C extends B{  
+void alevel(){System.out.println("Rukozo");}  
+} 
+class D extends C{
+    void university(){System.out.println("Rukozo");}
+} 
+public interface E { void leterE(){System.out.println("E");}
+}
+public class 
+class TestInheritance2{  
+public static void main(String args[]){  
+D d=new D();  
+d.primary();  
+d.olevel();  
+d.alevel();
+d.university();  
+}}  
